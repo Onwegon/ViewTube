1124641478d8313c297f748d74187b3542a3632d
mekhti9 <magerramov825@mail.ru>
2024-02-17 23:36:46 +0300

    Initial Commit

---
+++ ViewTube/ViewTubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewTubeApp.swift
+//  ViewTube
+//
+//  Created by mehdimagerramov on 17.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ViewTubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
